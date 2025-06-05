753cfd862f6462f66a71c4e405d1c41fc62e30e2
Khoirhilmi <khilmi924@gmail.com>
2025-06-05 16:33:23 +0700

    Add files via upload

---
+++ AktivitasPraktikum/Latihan/coba/Main.java
@@ -0,0 +1,13 @@
+package Aktivitas.coba;
+
+public class Main {
+    public static void main(String[] args) {
+        Smarthphone hp = new Smarthphone();
+
+        hp.hidupkan();
+        hp.cekBaterai();
+        
+        Gadget.info();
+        
+    }
+}
---
+++ AktivitasPraktikum/Latihan/coba/Smarthphone.java
@@ -0,0 +1,9 @@
+package Aktivitas.coba;
+
+public class Smarthphone implements Gadget {
+    
+    @Override
+    public void hidupkan() {
+        System.out.println("HP menyala");
+    }
+}
